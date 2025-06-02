7223e80d54faa3d003cef6347854c452a833cef0
MetsukiSutemi0979 <artemiymazur@yandex.ru>
2025-06-02 21:30:57 +0300

    Initial commit

---
+++ src/test/java/ru/netology/pages/DashboardPage.java
@@ -0,0 +1,9 @@
+package ru.netology.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class DashboardPage {
+    private SelenideElement buttonCredCard = $("")
+}
---
+++ src/test/java/ru/netology/utils/DBHelper.java
@@ -0,0 +1,23 @@
+package ru.netology.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBHelper {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+    }
+
+    public static void execute(String sql, Object... params) throws SQLException {
+        try (var conn = getConnection()) {
+            new QueryRunner().update(conn, sql, params);
+        }
+    }
+
+    public static void clearData() throws SQLException {
+        execute("DELETE FROM auth_codes;");
+        execute("DELETE FROM cards");
+        execute("DELETE FROM users;");
+    }
+}
